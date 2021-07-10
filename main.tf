49cc6c3203afdf80a48829e898089dfee1c038e5
unknown <docomowm@yahoo.co.jp>
2021-07-10 12:13:57 +0900

    main change4

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region = "ap-northeast-1"
+    access_key = var.aws_access_key
+    secret_key = var.aws_secret_key
+    region = "ap-northeast-1"
 }
 
 resource "aws_vpc" "test-vpc" {
