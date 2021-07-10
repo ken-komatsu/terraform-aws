f1bb172633df4ca8bbfa3b8fd4a80ae2b94473de
unknown <docomowm@yahoo.co.jp>
2021-07-10 12:55:09 +0900

    main change3

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   version = "~> 2.0"
   region = "${var.provider_region}"
   access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
+  secret_key = "Gj/1jiR/hKNRzU9LY0PTZMwfT5vrlQLW3sQlq9Vo"
 }
 
 resource "aws_vpc" "test-vpc" {
