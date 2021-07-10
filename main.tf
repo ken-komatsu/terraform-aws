572d5636bedef64ca0f525939f180b328790ba25
unknown <docomowm@yahoo.co.jp>
2021-07-10 12:16:08 +0900

    main change4

--- main.tf
+++ main.tf
@@ -1,9 +1,6 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-
 provider "aws" {
-    access_key = var.aws_access_key
-    secret_key = var.aws_secret_key
+    access_key = "AKIAZZSJDLM6KXLTF4XS" 
+    secret_key = "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"
     region = "ap-northeast-1"
 }
 
