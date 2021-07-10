26f40f06ac80988b74c1e25d8efd28b459f4008d
unknown <docomowm@yahoo.co.jp>
2021-07-10 12:51:29 +0900

    main change2

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 provider "aws" {
   version = "~> 2.0"
   region = "${var.provider_region}"
-  access_key = "${var.secret_access_key}"
+  access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
 }
 
