3629038dd60b7b53e356fc229af04352bd8ce605
unknown <docomowm@yahoo.co.jp>
2021-07-10 12:02:01 +0900

    main change2

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ variable "aws_secret_key" {}
 
 # definition provider
 provider "aws" {
-  version = "~> 2.0"
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   region = "ap-northeast-1"
