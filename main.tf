671f4066eeee729f265a40c3929fa8971cbee64b
unknown <docomowm@yahoo.co.jp>
2021-07-13 07:31:09 +0900

    modified:   main.tf

--- main.tf
+++ main.tf
@@ -13,3 +13,12 @@ resource "aws_vpc" "test-vpc" {
      Name = "test-vpc"
    }
 }
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c3fd0f5d33134a76"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "example"
+  }
+}
