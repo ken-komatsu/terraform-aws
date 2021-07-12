aa78296acd7c7906f8dcbbaff8284eedc507033a
unknown <docomowm@yahoo.co.jp>
2021-07-13 08:45:25 +0900

    modify 1

--- main.tf
+++ main.tf
@@ -14,3 +14,17 @@ resource "aws_vpc" "main" {
     Name = "handson"
   }
 }
+
+resource "aws_subnet" "public_1a" {
+  # 先程作成したVPCを参照し、そのVPC内にSubnetを立てる
+  vpc_id = "${aws_vpc.main.id}"
+
+  # Subnetを作成するAZ
+  availability_zone = "ap-northeast-1a"
+
+  cidr_block        = "10.0.1.0/24"
+
+  tags = {
+    Name = "handson-public-1a"
+  }
+}
